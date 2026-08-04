4ba108ab5d1cba6e83eb6ce2114ae1e3ae4631af
Urvee S Bettadapur <bettadapurvee@gmail.com>
2026-08-04 19:22:30 +0530

    Committed frontend

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from backend.routes.recommendation import router as recommendation_router
 from backend.routes.learning_path import router as learning_path_router
@@ -6,9 +7,43 @@ from backend.routes.learning_path import router as learning_path_router
 
 
 app = FastAPI(
+
     title="Learning Recommendation System API",
+
     version="1.0.0",
+
     description="API for course recommendation and adaptive learning path optimization."
+
+)
+
+
+
+# Allow React frontend to communicate with FastAPI backend
+
+app.add_middleware(
+
+    CORSMiddleware,
+
+    allow_origins=[
+
+        "http://localhost:5173"
+
+    ],
+
+    allow_credentials=True,
+
+    allow_methods=[
+
+        "*"
+
+    ],
+
+    allow_headers=[
+
+        "*"
+
+    ]
+
 )
 
 
@@ -17,23 +52,38 @@ app = FastAPI(
 def home():
 
     return {
+
         "message": "Welcome to the Learning Recommendation System API!"
+
     }
+
+
 
 @app.get("/health")
 def health():
+
     return {
+
         "status": "ok"
+
     }
 
+
+
 # Existing course recommendation API
+
 app.include_router(
+
     recommendation_router
+
 )
 
 
 
-# New adaptive learning path API
+# Adaptive learning path API
+
 app.include_router(
+
     learning_path_router
+
 )
